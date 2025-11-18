227971bb0d5be340bfbe4bed6f4578f4d5151039
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-11-18 20:33:18 +0200

    chore: git configs

---
+++ dot_gitconfig.aliases/amend_and_push.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+amend_and_push() {
+  local add_args=() c_args=() psh_args=()
+  local section=0
+  for arg in "$@"; do
+    case "$arg" in
+    --)
+      section=$((section + 1))
+      ;;
+    *)
+      case $section in
+      0) add_args+=("$arg") ;;
+      1) c_args+=("$arg") ;;
+      2) psh_args+=("$arg") ;;
+      esac
+      ;;
+    esac
+  done
+
+  echo "Calling: " >&2
+  echo git add "${add_args[@]}" >&2
+  echo git c --amend "${c_args[@]}" >&2
+  echo git psh --force-with-lease "${psh_args[@]}" >&2
+  echo "---" >&2
+
+  git add "${add_args[@]}"
+  git c --amend "${c_args[@]}" &&
+    git psh --force-with-lease "${psh_args[@]}"
+}

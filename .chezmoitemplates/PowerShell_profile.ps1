7eb245ce62e21e550929297fbe0e314f6b73efbc
Dragos Cirjan <dragos.cirjan@gmail.com>
2025-12-11 07:13:19 +0200

    chore: fixing pwsh profile

--- .chezmoitemplates/PowerShell_profile.ps1
+++ .chezmoitemplates/PowerShell_profile.ps1
@@ -1,3 +1,7 @@
 $UserHome = if ($IsWindows) { $env:USERPROFILE } else { $env:HOME }
+if (-not $UserHome) {
+    $UserHome = if ($env:USERPROFILE) { $env:USERPROFILE } else { $env:HOME }
+}
+
 $OhMyPoshConfig = Join-Path $UserHome 'ohmyposh.config.toml'
 oh-my-posh init pwsh --config $OhMyPoshConfig | Invoke-Expression
